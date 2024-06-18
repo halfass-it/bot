cf682a29e00193860fbf7a4889a94ec93bbf3480
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-18 18:52:23 +0100

    sync

---
+++ bin/__main__.py
@@ -0,0 +1,30 @@
+import sys
+
+from jsonargparse import CLI
+
+from bot.bot import Bot
+
+
+class Main:
+  def __init__(self, ip: str, port: str, cache_dir: str) -> None:
+    self.ip: str = ip
+    self.port: int = int(port)
+    self.cache_dir: str = cache_dir
+
+  def run(self):
+    bot = Bot(self.ip, self.port, self.cache_dir)
+    bot.start()
+
+def main():
+  try:
+    CLI(Main)
+    return 0
+  except KeyboardInterrupt:
+    return 0
+  except ValueError:
+    return 0
+
+
+if __name__ == '__main__':
+  main()
+  sys.exit(main())
---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/halfass-it/bot"
+VENV="$CACHE/.venv"
+POETRY="$VENV/bin/poetry"
+
+mkdir -p "$CACHE"
+if [ ! -d "$VENV" ]; then
+    $POETRY env use "$VENV"
+    $POETRY install
+fi
+
+$POETRY run main "127.0.0.1" "4156" "$CACHE"
